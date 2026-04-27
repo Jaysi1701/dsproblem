514a219d2f51754aaf598ea2a3d17c60c8651892
jaysi1701 <jaysigaur9449@gmail.com>
2026-04-27 16:45:54 +0530

    Added StackQueue UC4

--- src/stackqueue/Queue.java
+++ src/stackqueue/Queue.java
@@ -4,8 +4,14 @@ public class Queue<T> {
 
     LinkedList<T> list = new LinkedList<>();
 
+    // Insert at end
     public void enqueue(T data) {
         list.append(data);
+    }
+
+    // Remove from beginning
+    public T dequeue() {
+        return list.pop();
     }
 
     public void printQueue() {
