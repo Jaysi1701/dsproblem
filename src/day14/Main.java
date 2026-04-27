21dfcf834366dc5060717d34f3263664727a25a3
jaysi1701 <jaysigaur9449@gmail.com>
2026-04-27 16:28:23 +0530

    Added LinkedList UC2

--- src/day14/Main.java
+++ src/day14/Main.java
@@ -1,12 +1,18 @@
 package day14;
 
 public class Main {
+
     public static void main(String[] args) {
 
         LinkedList<Integer> list = new LinkedList<>();
 
+        // First node 70
         list.add(70);
+
+        // Add 30 to 70
         list.add(30);
+
+        // Add 56 to 30
         list.add(56);
 
         list.printList();
